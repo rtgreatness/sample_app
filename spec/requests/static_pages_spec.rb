21a76bae5d9592120a1de75e02ae8363dfdbf9c7
Robert Tsui <rtsui001@gmail.com>
2013-06-13 05:52:17 -0700

    Change to Ruby 1.9-style hash syntax

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,7 +12,7 @@ describe "Static Pages" do
 		it "should have the base title" do
 			visit '/static_pages/home'
 			page.should have_selector('title', 
-												:text => "Ruby on Rails Tutorial Sample App | Home")
+												:text => "Ruby on Rails Tutorial Sample App")
 		end
 
 		it "should not have a custom page title" do
