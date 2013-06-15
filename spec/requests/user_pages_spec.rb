1fb93437e0bbb0cb67895ee1a8ab0b1d9ca1503c
Robert Tsui <rtsui001@gmail.com>
2013-06-15 06:47:25 -0700

    Finish layout and routes

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User pages" do
 
-	subject { page }
+  subject { page }
 
-	describe "signup page" do
-		before { visit signup_path }
+  describe "signup page" do
+    before { visit signup_path }
 
-		it { should have_selector('h1', text: 'Sign Up') }
-		it { should have_selector('title', text: full_title('Sign Up')) }
-	end
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
+  end
 end
